aba534deff108c339e383f62e870bb6843b83b9c
kingsley78 <85633140+kingsley78@users.noreply.github.com>
2021-07-08 07:55:04 +0100

    Add files via upload

---
+++ flow3.py
@@ -0,0 +1,8 @@
+#fowchart3
+name = input("please enter name")
+if name == " alice":
+    print("hi alice")
+else:
+    age = int(input("please enter age"))
+    if age < 12:
+        print("you are not alice, kiddo")
